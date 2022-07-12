1121c3e7480ff1d83f18ca9ac961e2b34967a710
André Alves <andre.ats.profissional@gmail.com>
2022-07-11 22:03:06 -0300

    Ajustes 2

--- QuemSouEu-main/QuemSouEu/src/components/Dica.jsx
+++ QuemSouEu-main/QuemSouEu/src/components/Dica.jsx
@@ -1,6 +1,6 @@
 const Dica = ({ person, num }) => {
     return (
-        <div><p id="dica">{num}ª Dica: {person.dica01}</p></div>
+        <div><p id="dica">{num}ª Dica: {person}</p></div>
     )
 }
 
